e8060b03bb6f44380faecada3310bb6c6ffdffd6
Centurion <mrelectricguitar7@gmail.com>
2025-05-26 11:57:41 +0300

    Update index.js. Iteration 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,26 @@
-const cardList = document.querySelector('.places__list');
+const cardContainer = document.querySelector('.places__list');
 const template = document.querySelector('#card-template').content;
 
-function createCard(cardText, cardLink) {
-  const card = template.cloneNode(true);
-  const removeButton = card.querySelector('.card__delete-button');
-  card.querySelector('.card__title').textContent = cardText;
-  card.querySelector('.card__image').src = cardLink;
-  removeButton.addEventListener('click', removeCard);
-  return card;
-}
+function createCard(item, {deleteCard}) {
+  const cardElement = template.cloneNode(true);
+  const cardTitle = cardElement.querySelector('.card__title');
+  const cardLink = cardElement.querySelector('.card__image');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  
+  cardTitle.textContent = item.name;
+  cardLink.src = item.link;
+  cardLink.alt = `${item.name}, красивая фотография.`;
 
-function removeCard(evt) {
-  evt.target.closest('.places__item').remove();
+  deleteButton.addEventListener('click', deleteCard);
+
+  return cardElement;
 }
 
 initialCards.forEach(element => {
-  cardList.append(createCard(element.name, element.link));
+  const card = createCard(element, {
+    deleteCard: evt => {
+      evt.target.closest('.places__item').remove()
+    }
+  });
+  cardContainer.append(card);
 })
